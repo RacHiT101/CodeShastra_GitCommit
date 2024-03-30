1febcb6aa87d3a688c9a93c4d4a6a63f4e513d25
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2024-03-30 11:45:39 +0530

    f

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const jobController = require('../controllers/jobController');
 
 router.post('/jobs', jobController.createJob);
+router.post('/jobs/bulk', jobController.createMultipleJobs);
+router.get('/jobs', jobController.getAllJobs);
 
 module.exports = router;
