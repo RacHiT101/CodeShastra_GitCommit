8ece65d4fa1c8a9179b46a514e519168a4daf6d6
AdityaSurve <surveaditya521@gmail.com>
2024-03-30 10:20:48 +0530

    main ui

--- client/src/pages/MainModule.jsx
+++ client/src/pages/MainModule.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "../components/main/Sidebar";
+import Navbar from "../components/main/Navbar";
 
 const MainModule = () => {
-  return <div>MainModule</div>;
+  const [activeSection, setActiveSection] = useState("dashboard");
+  const getSection = () => {
+    switch (activeSection) {
+      case "dashboard":
+        return <div>dashboard</div>;
+      case "jobs":
+        return <div>jobs</div>;
+      case "profile":
+        return <div>profile</div>;
+      case "settings":
+        return <div>settings</div>;
+    }
+  };
+
+  return (
+    <div className="w-full h-full overflow-y-auto overflow-x-hidden flex items-center justify-center">
+      <Sidebar />
+
+      <div className="flex flex-col h-full w-full">
+        <Navbar />
+        {getSection()}
+      </div>
+    </div>
+  );
 };
 
 export default MainModule;
